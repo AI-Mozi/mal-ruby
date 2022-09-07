7d9d73fa8e3be2864819fce066a5bfe94da39bdf
AI-Mozi <bieniek.milosz0@gmail.com>
2022-09-07 16:35:36 +0200

    printer upgrades

--- impls/ruby/printer.rb
+++ impls/ruby/printer.rb
@@ -1,8 +1,18 @@
+require_relative "types"
+
 def pr_str(val)
-  case val.class
-  when Array
-    val.each { pr_str(_1) }
-  when nil
+  klass = val.class
+
+  case 
+  when klass == List
+    "(" + val.join(" ") + ")"
+  when klass == Vector
+    "[" + val.join(" ") + "]"
+  when klass == Hash
+    key_val = []
+    val.each { |k,v| key_val.push(k).push(v) }
+    "{" + key_val.join(" ") + "}"
+  when klass == NilClass
     "nil"
   else
     val.to_s
