1f80db0a5cbc0a7f8912b68f2e4d112f8317c04d
AI-Mozi <bieniek.milosz0@gmail.com>
2022-09-12 11:51:52 +0200

    step3

--- impls/ruby/step3_env.rb
+++ impls/ruby/step3_env.rb
@@ -2,32 +2,44 @@
 require_relative 'reader'
 require_relative 'printer'
 require_relative 'types'
+require_relative 'env'
 
-@repl_env = {
+@env = Env.new(nil)
+numeric_funcs = {
   '+': ->(a, b) { a + b },
   '-': ->(a, b) { a - b },
   '*': ->(a, b) { a * b },
   '/': ->(a, b) { (a / b).to_i }
 }
-
-public
+numeric_funcs.each { |k, v| @env.set(k, v) }
 
 def READ(val)
   read_str(val)
 end
 
-def EVAL(val, repl_env)
+def EVAL(val, env)
   if val.instance_of?(List)
     return val if val.size.zero?
 
-    new_list = eval_ast(val, repl_env)
-    args = new_list[1..-1]
-    calc = new_list[0]
+    case val[0]
+    when :def!
+      return env.set(val[1], EVAL(val[2], env))
+    when :"let*"
+      new_env = Env.new(@env)
+      val[1].each_slice(2) do |f, s|
+        new_env.set(f, EVAL(s, new_env))
+      end
+      return EVAL(val[2], new_env)
+    else
+      new_list = eval_ast(val, env)
+      args = new_list[1..]
+      calc = new_list[0]
 
-    return calc.call(*args) if calc.instance_of?(Proc)
+      return calc.call(*args) if calc.instance_of?(Proc)
+    end
   end
 
-  eval_ast(val, repl_env)
+  eval_ast(val, env)
 end
 
 def PRINT(val)
@@ -35,7 +47,7 @@ def PRINT(val)
 end
 
 def rep(input)
-  PRINT(EVAL(READ(input), @repl_env))
+  PRINT(EVAL(READ(input), @env))
 end
 
 def prompt(*args)
@@ -43,23 +55,26 @@ def prompt(*args)
   gets
 end
 
-def eval_ast(ast, struct)
-  if ['+', '-', '/', '*'].include?(ast)
-    ast = ast.to_sym
-    raise 'key not found' if struct[ast].nil?
-
-    struct[ast]
-  elsif ast.instance_of?(List)
-    List.new(ast.map { |a| EVAL(a, struct) })
-  elsif ast.instance_of?(Vector)
-    Vector.new(ast.map { |a| EVAL(a, struct) })
-  elsif ast.instance_of?(Hash)
-    Hash[ast.map { |k, v| [k, EVAL(v, struct)] }]
+def eval_ast(ast, env)
+  case ast
+  when Symbol
+    env.get(ast)
+  when List
+    List.new(ast.map { |a| EVAL(a, env) })
+  when Vector
+    Vector.new(ast.map { |a| EVAL(a, env) })
+  when Hash
+    Hash[ast.map { |k, v| [k, EVAL(v, env)] }]
   else
     ast
   end
 end
 
 while input = prompt('user> ')
-  puts rep(input)
+  begin
+    puts rep(input)
+  rescue Exception => e
+    puts "Error: #{e}"
+    puts "\t#{e.backtrace.join("\n\t")}"
+  end
 end
