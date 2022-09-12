496e52024e5c6996ebd3ca8071ab38a9a94b0c8b
AI-Mozi <bieniek.milosz0@gmail.com>
2022-09-12 11:51:12 +0200

    printer improves

--- impls/ruby/printer.rb
+++ impls/ruby/printer.rb
@@ -1,19 +1,17 @@
 require_relative "types"
 
 def pr_str(val)
-  klass = val.class
-
-  case 
-  when klass == List
-    "(" + val.map { |v| pr_str(v) }.join(" ") + ")"
-  when klass == Vector
-    "[" + val.map { |v| pr_str(v) }.join(" ") + "]"
-  when klass == Hash
+  case val
+  when List
+    "(#{val.map { |v| pr_str(v) }.join(' ')})"
+  when Vector
+    "[#{val.map { |v| pr_str(v) }.join(' ')}]"
+  when Hash
     key_val = []
-    val.each { |k,v| key_val.push(pr_str(k)).push(pr_str(v)) }
-    "{" + key_val.join(" ") + "}"
-  when klass == NilClass
-    "nil"
+    val.each { |k, v| key_val.push(pr_str(k)).push(pr_str(v)) }
+    "{#{key_val.join(' ')}}"
+  when NilClass
+    'nil'
   else
     val.to_s
   end
