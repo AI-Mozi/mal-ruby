6605d7308d25d3ea218830cc0d2f2ea39de34f50
AI-Mozi <bieniek.milosz0@gmail.com>
2022-09-13 10:33:26 +0200

    create core functions

---
+++ impls/ruby/core.rb
@@ -0,0 +1,19 @@
+$ns = {
+  '+': ->(a, b) { a + b },
+  '-': ->(a, b) { a - b },
+  '*': ->(a, b) { a * b },
+  '/': ->(a, b) { (a / b).to_i },
+  'pr-str': ->(*args) { args.map { |x| pr_str(x, true) }.join(' ') },
+  'str': ->(*args) { args.map { |x| pr_str(x, false) }.join('') },
+  'prn': ->(*args) { puts(args.map { |x| pr_str(x, true) }.join(' ')) },
+  'println': ->(*args) { puts(args.map { |x| pr_str(x, false) }.join('')) },
+  'list': ->(*args) { List.new(args) },
+  'list?': ->(a) { a.instance_of? List },
+  'empty?': ->(a) { a.empty? },
+  'count': ->(a) { a&.count || 0 },
+  '=': ->(a, b) { a == b },
+  '<': ->(a, b) { a < b },
+  '<=': ->(a, b) { a <= b },
+  '>': ->(a, b) { a > b },
+  '>=': ->(a, b) { a >= b }
+}
