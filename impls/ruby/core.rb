a5d74a836938be9a338aec66ba7ef2264a7e9a8e
AI-Mozi <bieniek.milosz0@gmail.com>
2022-09-15 10:44:57 +0200

    atoms methods

--- impls/ruby/core.rb
+++ impls/ruby/core.rb
@@ -1,19 +1,31 @@
+require_relative 'env'
+
 $ns = {
+  'pr-str': ->(*a) { a.map { |x| pr_str(x, true) }.join(' ') },
+  'str': ->(*a) { a.map { |x| pr_str(x, false) }.join('') },
+  'prn': ->(*a) { puts(a.map { |x| pr_str(x, true) }.join(' ')) },
+  'println': ->(*a) { puts(a.map { |x| pr_str(x, false) }.join(' ')) },
+  'read-string': ->(a) { read_str(a) },
+  'slurp': ->(a) { return File.read(a) },
+
+  'list': ->(*a) { List.new(a) },
+  'list?': ->(a) { a.instance_of? List },
+  'empty?': ->(a) { a.empty? },
+  'count': ->(a) { a&.count || 0 },
+
   '+': ->(a, b) { a + b },
   '-': ->(a, b) { a - b },
   '*': ->(a, b) { a * b },
   '/': ->(a, b) { (a / b).to_i },
-  'pr-str': ->(*args) { args.map { |x| pr_str(x, true) }.join(' ') },
-  'str': ->(*args) { args.map { |x| pr_str(x, false) }.join('') },
-  'prn': ->(*args) { puts(args.map { |x| pr_str(x, true) }.join(' ')) },
-  'println': ->(*args) { puts(args.map { |x| pr_str(x, false) }.join(' ')) },
-  'list': ->(*args) { List.new(args) },
-  'list?': ->(a) { a.instance_of? List },
-  'empty?': ->(a) { a.empty? },
-  'count': ->(a) { a&.count || 0 },
   '=': ->(a, b) { a == b },
   '<': ->(a, b) { a < b },
   '<=': ->(a, b) { a <= b },
   '>': ->(a, b) { a > b },
-  '>=': ->(a, b) { a >= b }
+  '>=': ->(a, b) { a >= b },
+
+  'atom': ->(a) { Atom.new(a) },
+  'atom?': ->(a) { a.instance_of? Atom },
+  'deref': ->(a) { a.val },
+  'reset!': ->(a, b) { a.val = b },
+  'swap!': ->(*a) { a[0].val = a[1].call(*[a[0].val].concat(a[2..])) }
 }
